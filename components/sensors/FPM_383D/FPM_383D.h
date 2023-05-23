cc2de9d9cb0a50956aaf95f7a58a97cca72dde65
huiyeruzhou <2251262734@qq.com>
2023-05-23 17:25:07 +0800

    驱动的C++适配

--- components/sensors/FPM_383D/FPM_383D.h
+++ components/sensors/FPM_383D/FPM_383D.h
@@ -1,10 +1,16 @@
 #pragma once
 #include "serial.h"
 #include "esp_log.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
 /**
  * @brief 触发一次指纹匹配
  * @param id 用于存储匹配到的ID的指针
  * @param score 用于存储匹配到的分数的指针
  * @return 返回状态码, 0x00代表成功
 */
-uint8_t PS_SearchMB(uint16_t *id, uint16_t *score);
+    uint8_t PS_SearchMB(uint16_t *id, uint16_t *score);
+#ifdef __cplusplus
+}
+#endif
