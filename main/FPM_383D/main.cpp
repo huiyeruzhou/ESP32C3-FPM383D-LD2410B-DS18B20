422c52662262cf866bf2cf996e50eefe3f269cd5
huiyeruzhou <2251262734@qq.com>
2023-05-23 17:25:31 +0800

    FPM383D的客户端和服务端程序

---
+++ main/FPM_383D/main.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "serial.h"
+#include "FPM_383D/FPM_383D.h"
+#include "server/simple_server.hpp"
+#include "sensor.pb.hpp"
+#include <unistd.h>
+#include "config_wifi.h"
+
+
+static const char *TAG = "FPM_383D Server";
+extern "C" void app_main(void) {
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    //Initialize wifi
+    // ip_set("192.168.1.120", "255.255.255.0", "192.168.1.1");
+    wifi_init_sta("yu", "esp32c3.");
+
+    //Initialize serial
+    serial_init(57600);
+
+    //Initialize RPC
+    auto server = new erpc::SimpleServer("localhost", 12345);
+    class myService :public sensor_SensorService_Service {
+    private:
+        bool start;
+    public:
+        rpc_status open(sensor_Empty *req, sensor_Empty *rsp) override {
+            ESP_LOGI(TAG, "open");
+            start = true;
+            return rpc_status::Success;
+        }
+        rpc_status close(sensor_Empty *req, sensor_Empty *rsp) override {
+            ESP_LOGI(TAG, "close");
+            if (start) {
+                start = false;
+            }
+            return rpc_status::Success;
+        }
+        rpc_status read(sensor_Empty *req, sensor_Value *rsp) override {
+            ESP_LOGI(TAG, "read");
+            if (!start) {
+                rsp->value = -1;
+                return rpc_status::Fail;
+            }
+            uint16_t id = 0, score = 0;
+            int status = PS_SearchMB(&id, &score);
+            if (status) {
+                ESP_LOGE(TAG, "Failed to recognize finger, status=%d", status);
+                return rpc_status::Fail;
+            }
+            else if (id == 0xFFFF) {
+                ESP_LOGW(TAG, "Macth Failed, score=%d", score);
+                rsp->value = (float) id;
+                return rpc_status::Fail;
+            }
+            else {
+                ESP_LOGW(TAG, "id=%d, score=%d", id, score);
+                rsp->value = (float) id;
+                return rpc_status::Success;
+            }
+        }
+        rpc_status configure(sensor_Value *req, sensor_Empty *rsp) override {
+            ESP_LOGI(TAG, "configure");
+            /*some configure*/
+            return rpc_status::Success;
+        }
+    };
+    auto service = new myService();
+    server->addService(service);
+    server->open();
+
+    /* run server */
+    server->run();
+}
--- main_client/client.cpp
+++ main_client/client.cpp
@@ -6,18 +6,29 @@
 int main(int argc, char **argv) {
     // system("sudo ifconfig eth0 192.168.0.100 netmask 255.255.255.0");
     /* eRPC client side initialization */
-    auto client = new sensor_SensorService_Client("192.168.93.194", 12345);
+    auto client = new sensor_SensorService_Client("192.168.93.151", 12345);
     /* code */
-    int32_t ret = 0;
+    int32_t ret = 1;
     if (rpc_status::Success != client->open()) return -1;
     sensor_Empty empty;
     sensor_Status status;
     sensor_Value value;
+    int sensor = 1;
     client->open(&empty, &empty);
     printf("response: %" PRId16 "\n", status.status);
     for (;;) {
         client->read(&empty, &value);
-        printf("distance: %f\n", value.value);
+        switch (sensor) {
+        case 0:/*DS18B20*/
+            printf("temperature: %f\n", value.value);
+            break;
+        case 1:/*FPM_383D*/
+            printf("finger id: %f\n", value.value);
+            break;
+        case 2:/*LD2410B*/
+            printf("distance: %f\n", value.value);
+            break;
+        }
         sleep(1);
     }
     return 0;
